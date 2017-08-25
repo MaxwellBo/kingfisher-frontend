a9580aca5b889c316f50f5b12fa607a16ca29623
Max Bo <max@maxbo.me>
2017-08-25 14:49:16 +1000

    Track login status

--- src/Login.tsx
+++ src/Login.tsx
@@ -8,6 +8,13 @@ interface State {}
 interface Props {}
 
 const authUi = new firebaseui.auth.AuthUI(firebase.auth());
+firebase.auth().onAuthStateChanged(authStateChangedCallback);
+
+var user: firebase.User | null = null;
+
+function authStateChangedCallback(u: firebase.User) {
+  user = u;
+}
 
 class Login extends React.Component<Props, State> {
    componentWillUnmount() {
@@ -24,13 +31,18 @@ class Login extends React.Component<Props, State> {
         tosUrl: '<your-tos-url>'
       };
 
-      authUi.start('#firebaseui-auth-container', uiConfig);
+      if (!user) {
+        authUi.start('#Login-firebaseui-auth-container', uiConfig);
+      }
   }
 
   render() {
-    return (
-      <div id="firebaseui-auth-container" />
-    );
+    if (user) {
+      // authUi.reset(); // You might need to do this
+      return (<div id="Login-info"><p>You're already logged in!</p></div>);
+    } else {
+      return (<div id="Login-firebaseui-auth-container" />);
+    }
   }
 }
 

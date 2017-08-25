b2ce983a1b87de4c0b5e7b592b5bf0ace711f0a3
Max Bo <max@maxbo.me>
2017-08-25 17:30:27 +1000

    Add TOS Component

---
+++ src/TOS.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.css';
+
+class TOS extends React.PureComponent<{}, {}> {
+  render() {
+    return (
+      <div className="TOS">
+        <h1>Terms of Service</h1>
+        <ol>
+          <li>TODO</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default TOS;

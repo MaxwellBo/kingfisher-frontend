e21eab989e3136380658642e23a5b29a566050e8
Max Bo <max@maxbo.me>
2017-08-28 19:26:26 +1000

    Stub out firebase database push

--- src/Export.tsx
+++ src/Export.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
+import * as firebase from 'firebase';
 import './App.css';
 
+function writeUserData() {
+  const user = firebase.auth().currentUser;
+
+  if (user) {
+    firebase.database().ref('users/' + user.uid).set({
+      uid: user.uid,
+      displayName: user.displayName,
+      email: user.email
+    });
+  }
+}
+
 class Export extends React.PureComponent<{}, {}> {
+
+  handleWriteUserDataClick = () => {
+    writeUserData();
+  }
+
   render() {
     return (
       <div className="Export">
         <p>Export</p>
+        <button onClick={this.handleWriteUserDataClick}>Write user data</button>
       </div>
     );
   }

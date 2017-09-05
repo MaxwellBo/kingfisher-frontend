7ae04e77a1a2ccb8a57fd957deedc97141fc5e82
Max Bo <max@maxbo.me>
2017-09-05 11:15:04 +1000

    WILL AMEND

--- kingfishernative/App.js
+++ kingfishernative/App.js
@@ -12,10 +12,7 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentPage: "index",
       activeSite: "000000",
-      titleInfo: "Placeholder",
-      pageHistory: []
     };
     this.changePage = this.changePage.bind(this);
     this.changeActiveSite = this.changeActiveSite.bind(this);
@@ -30,13 +27,6 @@ export default class App extends React.Component {
     });
   }
 
-  changePage(pageName) {
-    this.setState({
-      pageHistory: this.state.pageHistory.concat([this.state.currentPage]),
-      currentPage: pageName
-    })
-    console.log(this.state.pageHistory)
-  }
 
   changeActiveSite(siteCode) {
     this.setState({
@@ -44,26 +34,12 @@ export default class App extends React.Component {
     })
   }
 
-  goBack() {
-    let copyHistory = this.state.pageHistory;
-    let prevPage = this.state.pageHistory[this.state.pageHistory.length - 1]
-    copyHistory.splice(copyHistory.length - 1, 1);
-    this.setState({
-      currentPage: prevPage,
-      pageHistory: copyHistory,
-    })
-  }
-
   render() {
     return (
       <View style={styles.app}>
         <AppScreen
-          currentPage={this.state.currentPage}
-          changePage={(pageName) => this.changePage(pageName)}
           changeActiveSite={(siteCode) => this.changeActiveSite(siteCode)}
           activeSite={this.state.activeSite}
-          titleInfo={this.state.titleInfo}
-          goBack={() => this.goBack()}
         />
       </View>
     )

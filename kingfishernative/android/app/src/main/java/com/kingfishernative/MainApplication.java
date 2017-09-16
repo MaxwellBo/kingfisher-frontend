da05dcf7f1f6a0b37481b779fed75e5f6a30121e
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-16 16:27:02 +1000

    Add a bunch of font assets, install nativebase, run `react-native link`

--- kingfishernative/android/app/src/main/java/com/kingfishernative/MainApplication.java
+++ kingfishernative/android/app/src/main/java/com/kingfishernative/MainApplication.java
@@ -3,6 +3,8 @@ package com.kingfishernative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,6 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAnalyticsPackage(),
         new RNFirebasePackage(),  // <-- Add this line
         new RNFirebaseAuthPackage(),
         new RNFirebaseDatabasePackage()

b91a1e557abedd795767731de25dcddbd0a8c8b4
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-19 08:49:42 +1000

    Improve layout of landing page

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,10 @@ interface CardProps {
 
 function Card(cardProps: CardProps) {
   return (
-    <div className="card">
-      <div className="card-image">
+    <div className="card centered">
+      <div className="card-image-container">
         <figure className="image is-4by3">
-          <img src={cardProps.imgSrc} alt={cardProps.imgAlt} />
+          <img className="card-image" src={cardProps.imgSrc} alt={cardProps.imgAlt} />
         </figure>
       </div>
       <div className="card-content">
@@ -111,7 +111,7 @@ function Home() {
     
       </section>
       <section className="section">
-        <div className="columns">
+        <div className="columns is-8">
           <div className="column">
             <Card 
               title="Instant Backups." 
@@ -150,31 +150,33 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="section">
+      <section className="section centered">
         <h1 className="title">
           Product Video
         </h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/oZc_jrutGrk" />
+        <div className="centered">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/oZc_jrutGrk" />
+        </div>
       </section>
-      <section className="section">
+      <section className="section centered">
         <h1 className="title">
           Features
         </h1>
         <p>
           <ul>
-            <li>Automatically sync data with a centralised database of all tree measurements.</li>
-            <li>Visualise and compare data, online or offline.</li>
-            <li>Easy to use collection interfact.</li>
-            <li>Automatic geolocation when creating sites or tree measurements.</li>
-            <li>Get notified when the data you've collected is statistically significant enough to stop.</li>
+            <li><i className="fa fa-check" aria-hidden="true"/> Automatically sync data with a centralised database of all tree measurements.</li>
+            <li><i className="fa fa-check" aria-hidden="true"/> Visualise and compare data, online or offline.</li>
+            <li><i className="fa fa-check" aria-hidden="true"/> Easy to use collection interface.</li>
+            <li><i className="fa fa-check" aria-hidden="true"/> Automatic geolocation when creating sites or tree measurements.</li>
+            <li><i className="fa fa-check" aria-hidden="true"/> Get notified when the data you've collected is statistically significant enough to stop.</li>
           </ul>
         </p>
       </section>
-      <section className="section">
+      <section className="section centered">
         <h1 className="title">
           Why Use Kingfisher?
         </h1>
-        <p>Kingfisher's many features can help streamline the data collection process for your organisation. The 
+        <p className="column is-three-quarters centered">Kingfisher's many features can help streamline the data collection process for your organisation. The 
           app removes the need to manually transfer data into a spreadsheet or database, and will save you tons 
           of time doing collection or data entry alike. Not only that, but it's super safe, and you have no need
           to worry about losing your data between the site and the office. Everything is backed up on your phone 

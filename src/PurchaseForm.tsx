e9eafb4e25aee777875b16243de921caf2381067
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-19 09:34:46 +1000

    Get it redirecting on submit

--- src/PurchaseForm.tsx
+++ src/PurchaseForm.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Redirect } from 'react-router-dom';
 import * as firebase from 'firebase';
 
 interface State {
   name: string;
   email: string;
   occupation: string;
+  submitted: boolean;
 }
 
 interface Props {}
@@ -16,7 +17,8 @@ export default class PurchaseForm extends React.Component<Props, State> {
     this.state = {
       name: '',
       email: '',
-      occupation: '',
+      occupation: 'Student',
+      submitted: false,
     };
   }
 
@@ -41,71 +43,148 @@ export default class PurchaseForm extends React.Component<Props, State> {
     };
     console.log(visitor);
     firebase.database().ref('visitors').push().set(visitor);
+    this.setState({submitted: true});
   }
 
   render() {
-    return (
-      <div className="purchaseForm">
-        <section className="hero is-info is-medium">
-          <div className="hero-head">
-            <header className="nav">
-              <div className="container">
-                <div className="nav-left">
-                  <a className="nav-item">
-                    <img src="images/bulma-type-white.png" alt="Kingfisher" />
-                  </a>
-                </div>
-                <span className="nav-toggle">
-                  {/* <span></span>
-                  <span></span>
-                  <span></span> */}
-                </span>
-                <div className="nav-right nav-menu">
-                  {/* <a className="nav-item is-active"> */}
-                  <a className="nav-item" href="https://www.facebook.com/Kingfisher-127569781308370/">
-                    <i className="fa fa-facebook-official" aria-hidden="true"/>
-                  </a>
-                  <a className="nav-item" href="https://twitter.com/KingfisherData">
-                    <i className="fa fa-twitter-square" aria-hidden="true"/>
-                  </a>
-                  <a className="nav-item" href="https://www.instagram.com/kingfisherdata/">
-                    <i className="fa fa-instagram" aria-hidden="true"/>
-                  </a>
+    if (this.state.submitted) {
+      return (
+        <div className="purchaseForm">
+          <section className="hero is-info is-medium">
+            <div className="hero-head">
+              <header className="nav">
+                <div className="container">
+                  <div className="nav-left">
+                    <a href="/" className="nav-item">
+                      <img src="images/bulma-type-white.png" alt="Kingfisher" />
+                    </a>
+                  </div>
+                  <span className="nav-toggle">
+                    {/* <span></span>
+                    <span></span>
+                    <span></span> */}
+                  </span>
+                  <div className="nav-right nav-menu">
+                    {/* <a className="nav-item is-active"> */}
+                    <a className="nav-item" href="https://www.facebook.com/Kingfisher-127569781308370/">
+                      <i className="fa fa-facebook-official" aria-hidden="true"/>
+                    </a>
+                    <a className="nav-item" href="https://twitter.com/KingfisherData">
+                      <i className="fa fa-twitter-square" aria-hidden="true"/>
+                    </a>
+                    <a className="nav-item" href="https://www.instagram.com/kingfisherdata/">
+                      <i className="fa fa-instagram" aria-hidden="true"/>
+                    </a>
 
-                  <a href="/purchase" className="nav-item">
-                    Purchase
-                  </a>
-                  <a href="https://api.uqcloud.net/login/http://deco3801-jquery-only.uqcloud.net/stats" className="nav-item">
-                    Statistics
-                  </a>
-                  <a className="nav-item">
-                    Documentation
-                  </a>
+                    <a href="/purchase" className="nav-item">
+                      Purchase
+                    </a>
+                    <a href="https://api.uqcloud.net/login/http://deco3801-jquery-only.uqcloud.net/stats" className="nav-item">
+                      Statistics
+                    </a>
+                    <a className="nav-item">
+                      Documentation
+                    </a>
+                  </div>
+                </div>
+              </header>
+            </div>
+          </section>
+          <section className="section">
+            <h1 className="title">
+              Thank you!
+            </h1>
+            <h2 className="subtitle">
+              Your response has been recorded.
+            </h2>
+          </section>
+        </div>
+      );
+    } else {
+      return (
+        <div className="purchaseForm">
+          <section className="hero is-info is-medium">
+            <div className="hero-head">
+              <header className="nav">
+                <div className="container">
+                  <div className="nav-left">
+                    <a href="/" className="nav-item">
+                      <img src="images/bulma-type-white.png" alt="Kingfisher" />
+                    </a>
+                  </div>
+                  <span className="nav-toggle">
+                    {/* <span></span>
+                    <span></span>
+                    <span></span> */}
+                  </span>
+                  <div className="nav-right nav-menu">
+                    {/* <a className="nav-item is-active"> */}
+                    <a className="nav-item" href="https://www.facebook.com/Kingfisher-127569781308370/">
+                      <i className="fa fa-facebook-official" aria-hidden="true"/>
+                    </a>
+                    <a className="nav-item" href="https://twitter.com/KingfisherData">
+                      <i className="fa fa-twitter-square" aria-hidden="true"/>
+                    </a>
+                    <a className="nav-item" href="https://www.instagram.com/kingfisherdata/">
+                      <i className="fa fa-instagram" aria-hidden="true"/>
+                    </a>
+
+                    <a href="/purchase" className="nav-item">
+                      Purchase
+                    </a>
+                    <a href="https://api.uqcloud.net/login/http://deco3801-jquery-only.uqcloud.net/stats" className="nav-item">
+                      Statistics
+                    </a>
+                    <a className="nav-item">
+                      Documentation
+                    </a>
+                  </div>
+                </div>
+              </header>
+            </div>
+          </section>
+          <section className="section centered">
+            <h1 className="title">Express Interest</h1>
+            <h2 className="subtitle">
+              We can't sell you the product right now, but we'll contact you as soon as we can!
+            </h2>
+            <div className="form centered">
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input className="input" type="text" value={this.state.name} 
+                    onChange={this.handleChangeName.bind(this)} placeholder="Text input"/>
                 </div>
               </div>
-            </header>
-          </div>
-        </section>
-        <section className="section">
-          <div className="form-row">
-            <span className="field-label">Name</span>
-            <input type="text" className="field" value={this.state.name} onChange={this.handleChangeName.bind(this)} />
-          </div>
-          <div className="form-row">
-            <span className="field-label">Email</span>
-            <input type="text" className="field"value={this.state.email} onChange={this.handleChangeEmail.bind(this)} />
-          </div>
-          <div className="form-row">
-            <span className="field-label">Occupation</span>
-            <input type="text" className="field"value={this.state.occupation} onChange={this.handleChangeOccupation.bind(this)} />
-          </div>
-          <div className="form-row">
-            <div className="button" onClick={this.writeFormData.bind(this)}>
-              Submit
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input className="input" type="text" value={this.state.email} 
+                    onChange={this.handleChangeEmail.bind(this)} placeholder="Text input"/>
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Occupation</label>
+                <div className="control centered">
+                  <div className="select">
+                    <select onChange={this.handleChangeOccupation.bind(this)}>
+                      <option>Student</option>
+                      <option>Teacher</option>
+                      <option>Researcher</option>
+                      <option>Other</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="form-row submit">
+                <div className="button" onClick={this.writeFormData.bind(this)}>
+                  Submit
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
-    );
+          </section>
+        </div>
+      );
+    }
   }
 }

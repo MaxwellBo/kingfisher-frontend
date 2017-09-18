638dd6c320d7054767ed0e5d86cd25732fff191e
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-18 22:53:13 +1000

    Basic Purchase Form

---
+++ src/PurchaseForm.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Link, Route } from 'react-router-dom';
+import * as firebase from 'firebase';
+
+export default class PurchaseForm extends React.Component<Props, State> {
+  render() {
+    return (
+      <div/>
+    )
+  }
+}

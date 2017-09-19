543a91f066dbfcf99ce82313496be485683a3530
Haziq <hbnordin2@gmail.com>
2017-09-19 11:22:50 +1000

    Added pie chart

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as firebase from 'firebase';
+import {PieChart, LineChart} from 'react-easy-chart';
+import any = jasmine.any;
 
 function writeVisitorData() {
 
@@ -39,6 +41,26 @@ class Stats extends React.Component<Props, State> {
     this.state.visitorsRef.off();
   }
 
+  getPieData(visitors: any) {
+    let data = {};
+    for(let key in visitors) {
+      let occupation = visitors[key]['occupation'];
+      if(occupation in data) {
+        data[occupation] += 1;
+      } else {
+        data[occupation] = 1;
+      }
+    }
+    console.log(data)
+      let parsable_data:any = []
+      for(let key in data) {
+      let log:any = {'key':key, 'value':data[key]}
+        parsable_data.push(log)
+      }
+      console.log(parsable_data)
+    return parsable_data
+  }
+
   render() {
     const { visitors } = this.state;
     let recentEOIs = (visitors == null) ? <div/> : Object.keys(visitors).map(key =>
@@ -57,6 +79,8 @@ class Stats extends React.Component<Props, State> {
       </div>)
     )
 
+      let pieData = this.getPieData(visitors)
+
     return (
       <div className="stats">
         <section className="section">
@@ -71,6 +95,11 @@ class Stats extends React.Component<Props, State> {
               </div>
               <div className="column is-half centered">
                 <p className="centered">EOIs Today</p>
+                <div>
+                  <PieChart
+                      data={pieData}
+                  />
+                </div>
               </div>
             </div>
           </div>
@@ -78,6 +107,30 @@ class Stats extends React.Component<Props, State> {
         <section className="section">
           <div className="container">
             <h1 className="title">Interest Over Time</h1>
+            <LineChart
+                xType={'time'}
+                axes
+                grid
+                verticalGrid
+                interpolate={'cardinal'}
+                width={750}
+                height={250}
+                data={[
+                    [
+                        { x: '1-Jan-15', y: 20 },
+                        { x: '1-Feb-15', y: 10 },
+                        { x: '1-Mar-15', y: 33 },
+                        { x: '1-Apr-15', y: 45 },
+                        { x: '1-May-15', y: 15 }
+                    ], [
+                        { x: '1-Jan-15', y: 10 },
+                        { x: '1-Feb-15', y: 15 },
+                        { x: '1-Mar-15', y: 13 },
+                        { x: '1-Apr-15', y: 15 },
+                        { x: '1-May-15', y: 10 }
+                    ]
+                ]}
+            />
           </div>
         </section>
         <section className="section">

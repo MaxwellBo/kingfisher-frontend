22a659f04c0765283f0ab107d340f43a5542a0e5
Max Bo <max@maxbo.me>
2017-09-19 13:18:08 +1000

    Refactor stats

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -2,25 +2,19 @@ import * as React from 'react';
 import * as firebase from 'firebase';
 import Nav from "./Nav";
 import any = jasmine.any;
-import {PieChart, LineChart} from 'react-easy-chart';
+import { PieChart, LineChart } from 'react-easy-chart';
 
-function writeVisitorData() {
-
-  const getCookiebyName = (name: String) => {
-    var pair = document.cookie.match(new RegExp(name + '=([^;]+)'));
-    return !!pair ? pair[1] : null;
-  };
-
-  firebase.database().ref('visitors').child('TODO').set({});
-}
-
-interface Props {}
+interface Props { }
 interface State {
   visitors: {};
   visitorsRef: firebase.database.Reference;
 }
 
 class Stats extends React.Component<Props, State> {
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
+
   constructor(props: Props) {
     super(props);
 
@@ -30,10 +24,21 @@ class Stats extends React.Component<Props, State> {
     };
   }
 
+  componentWillMount() {
+    const getCookiebyName = (name: String) => {
+      var pair = document.cookie.match(new RegExp(name + '=([^;]+)'));
+      return !!pair ? pair[1] : null;
+    };
+
+    if (getCookiebyName('EAIT_WEB') === null) {
+      this.context.router.history.push('/');
+    }
+
+  }
   componentDidMount() {
     this.state.visitorsRef.on('value', (visitors) => {
       if (visitors) {
-        this.setState({visitors: visitors.val()});
+        this.setState({ visitors: visitors.val() });
       }
     });
   }
@@ -44,75 +49,75 @@ class Stats extends React.Component<Props, State> {
 
   getPieData(visitors: any) {
     let data = {};
-    for(let key in visitors) {
+    for (let key in visitors) {
       let occupation = visitors[key]['occupation'];
-      if(occupation in data) {
+      if (occupation in data) {
         data[occupation] += 1;
       } else {
         data[occupation] = 1;
       }
     }
     console.log(data)
-      let parsable_data:any = []
-      for(let key in data) {
-      let log:any = {'key':key, 'value':data[key]}
-        parsable_data.push(log)
-      }
-      console.log(parsable_data)
+    let parsable_data: any = []
+    for (let key in data) {
+      let log: any = { 'key': key, 'value': data[key] }
+      parsable_data.push(log)
+    }
+    console.log(parsable_data)
     return parsable_data
   }
 
   getTimelineData(visitors: any) {
-      let data = {};
-      for(let key in visitors) {
-          let occupation = visitors[key]['date'];
-          if(occupation in data) {
-              data[occupation] += 1;
-          } else {
-              data[occupation] = 1;
-          }
+    let data = {};
+    for (let key in visitors) {
+      let occupation = visitors[key]['date'];
+      if (occupation in data) {
+        data[occupation] += 1;
+      } else {
+        data[occupation] = 1;
       }
-      console.log("timelinedata")
-      console.log(data)
-      console.log("timelinedata")
+    }
+    console.log("timelinedata")
+    console.log(data)
+    console.log("timelinedata")
 
-      let parsable_data:any = []
-      for(let key in data) {
-          let log:any = {'x':key, 'y':data[key]}
-          parsable_data.push(log)
-      }
+    let parsable_data: any = []
+    for (let key in data) {
+      let log: any = { 'x': key, 'y': data[key] }
+      parsable_data.push(log)
+    }
 
-      parsable_data.sort(function(a:any, b:any) {
-          var x = a['x'];
-          var y = b['x'];
-          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-      });
-      return parsable_data
+    parsable_data.sort(function (a: any, b: any) {
+      var x = a['x'];
+      var y = b['x'];
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+    return parsable_data
   }
 
-  getEoiToday(visitors:any) {
-      var today = new Date();
+  getEoiToday(visitors: any) {
+    var today = new Date();
 
-      let dates:Array<String> = [];
-      let count = 0;
-      for(let key in visitors) {
-          let date = visitors[key]['date'];
-          var arr = date.split("-");
-          let todayDate = "" + today.getDate();
-          let todayMonth = "" + today.getMonth();
-          let todayYear = "" + today.getFullYear();
+    let dates: Array<String> = [];
+    let count = 0;
+    for (let key in visitors) {
+      let date = visitors[key]['date'];
+      var arr = date.split("-");
+      let todayDate = "" + today.getDate();
+      let todayMonth = "" + today.getMonth();
+      let todayYear = "" + today.getFullYear();
 
-          if(arr[0] === todayDate && arr[1] === todayMonth && arr[2] === todayYear) {
-              count += 1;
-          }
+      if (arr[0] === todayDate && arr[1] === todayMonth && arr[2] === todayYear) {
+        count += 1;
       }
+    }
 
-      return count;
+    return count;
   }
 
   render() {
     const { visitors } = this.state;
-  let recentEOIs = (visitors == null) ? [<div key="1"/>] : Object.keys(visitors).map(key =>
+    let recentEOIs = (visitors == null) ? [<div key="1" />] : Object.keys(visitors).map(key =>
       (<div className="rowRecent" key={key}>
         <div className="columns">
           <div className="column">
@@ -134,15 +139,15 @@ class Stats extends React.Component<Props, State> {
     recentEOIs.reverse();
     recentEOIs.length = 10 // Chop the most recent 10 EOIs
 
-      let pieData = this.getPieData(visitors)
-      let lineData = this.getTimelineData(visitors)
-      this.getEoiToday(visitors);
-      let count = this.getEoiToday(visitors);
+    let pieData = this.getPieData(visitors)
+    let lineData = this.getTimelineData(visitors)
+    this.getEoiToday(visitors);
+    let count = this.getEoiToday(visitors);
 
     return (
       <div className="stats">
         <div className="hero-head">
-          <Nav/>
+          <Nav />
         </div>
         <section className="section">
           <div className="container">
@@ -157,10 +162,10 @@ class Stats extends React.Component<Props, State> {
               <div className="column is-half centered">
                 <div>
                   <PieChart
-                      labels
-                      data={pieData}
+                    labels
+                    data={pieData}
                   />
-                <div className="centered"><strong>Interest by Occupation</strong></div>
+                  <div className="centered"><strong>Interest by Occupation</strong></div>
                 </div>
               </div>
             </div>
@@ -170,14 +175,14 @@ class Stats extends React.Component<Props, State> {
           <div className="container">
             <h1 className="title">Interest Over Time</h1>
             <LineChart
-                xType={'text'}
-                axes
-                grid
-                verticalGrid
-                interpolate={'cardinal'}
-                width={750}
-                height={250}
-                data={[lineData]}
+              xType={'text'}
+              axes
+              grid
+              verticalGrid
+              interpolate={'cardinal'}
+              width={750}
+              height={250}
+              data={[lineData]}
             />
           </div>
         </section>
@@ -203,7 +208,7 @@ class Stats extends React.Component<Props, State> {
             {recentEOIs}
           </div>
         </section>
-        
+
       </div>
     );
   }

3af7c3545f341a83582b87efc55d1deab9c255c2
Hugo Kawamata <hugokawamata@gmail.com>
2017-09-20 18:23:08 +1000

    Attempt to get unit testing working, fail

--- kingfishernative/App.test.js
+++ kingfishernative/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import App from './App';
+import 'react-native';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });

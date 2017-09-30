bc1e3f1e60225d70ca0338f2d39ff52a3fab5c4c
Max Bo <max@maxbo.me>
2017-09-30 15:08:16 +1000

    Fix incorrectly named component

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default class Nav extends React.Component {
+export default class Footer extends React.Component {
   render() {
     return (
       <footer className="footer">

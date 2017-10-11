ca154d5eb33c2ac8965dcb131655fa55bf7aa214
Hugo Kawamata <hugokawamata@gmail.com>
2017-10-11 17:34:39 +1000

    Move “addsite” to settings page

--- kingfishernative/src/PageSettings.js
+++ kingfishernative/src/PageSettings.js
@@ -11,7 +11,56 @@ import AccordionViewSite from "./AccordionViewSite"
 import Field from "./Field"
 import PageVizTree from "./PageVizTree"
 
+class AddSite extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newSiteCode: "",
+    }
+  }
 
+  changeNewSiteCode = (spec, code) => {
+    obj = {}
+    obj[spec] = code
+    this.setState(obj);
+  }
+  
+  addNewSite = () => {
+    if (this.state.newSiteCode !== "") {
+      const ref = fbi.database().ref("sites").child(this.state.newSiteCode);
+
+      ref.keepSynced(true);
+
+      ref.set({measurements:""});
+      this.setState({ newSiteCode: "" });
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.siteAddLabel}>Add New Site</Text>
+        <View style={styles.siteAddCont}>
+          <Field
+            name="newSiteCode"
+            extraStyles={styles.siteAddField}
+            onChangeText={this.changeNewSiteCode}
+            />
+          <TouchableHighlight
+            onPress={() => {
+              this.addNewSite();
+              this.props.goBack()}
+            }
+            accessibilityLabel="Add New Site"
+            style={styles.siteAddButton}
+            >
+            <Text style={{fontSize: 20, textAlign: "center"}}>+</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    )
+  }
+}
 
 export default class PageSettings extends React.Component {
   constructor(props) {
@@ -24,6 +73,8 @@ export default class PageSettings extends React.Component {
         <View>
           <Text style={styles.pageHeadTitle}>Settings</Text>
         </View>
+
+        <AddSite goBack={this.props.history.goBack}/>
         <View style={styles.sites}>
         </View>
       </Content>
--- kingfishernative/src/PageSites.js
+++ kingfishernative/src/PageSites.js
@@ -11,6 +11,7 @@ import AccordionViewSite from "./AccordionViewSite"
 import Field from "./Field"
 import PageVizTree from "./PageVizTree"
 import PageSettings from "./PageSettings"
+import LinkButton from "./LinkButton"
 
 /**
  * Returns a single "site" button.
@@ -57,8 +58,8 @@ function Sites(props) {
     <Content contentContainerStyle={styles.pageCont}>
       <View>
         <Text style={styles.pageHeadTitle}>Sites</Text>
-        <Text style={styles.pageHeadDesc}>Add a new site record to get started.</Text>
-        <AddSite/>
+        <Text style={styles.pageHeadDesc}>Click to view past records, or add a new record.</Text>
+        <LinkButton to={"/sites/settings"} buttonText="Settings" />
       </View>
       <View style={styles.sites}>
         
@@ -66,53 +67,6 @@ function Sites(props) {
       </View>
     </Content>
   );
-}
-
-class AddSite extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      newSiteCode: "",
-    }
-  }
-
-  changeNewSiteCode = (spec, code) => {
-    obj = {}
-    obj[spec] = code
-    this.setState(obj);
-  }
-  
-  addNewSite = () => {
-    if (this.state.newSiteCode !== "") {
-      const ref = fbi.database().ref("sites").child(this.state.newSiteCode);
-
-      ref.keepSynced(true);
-
-      ref.set({measurements:""});
-      this.setState({ newSiteCode:"" });
-    }
-  }
-
-  render() {
-    return (
-      <View>
-        <View style={styles.siteAddCont}>
-          <Field
-            name="newSiteCode"
-            extraStyles={styles.siteAddField}
-            onChangeText={this.changeNewSiteCode}
-            />
-          <TouchableHighlight
-            onPress={this.addNewSite}
-            accessibilityLabel="Add New Site"
-            style={styles.siteAddButton}
-            >
-            <Text style={{fontSize: 20, textAlign: "center"}}>+</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    )
-  }
 }
 
 export default class PageSites extends React.Component {
